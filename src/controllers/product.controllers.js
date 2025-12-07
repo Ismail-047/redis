50285852174eb3b093282c7d262ab0bb89e08578
Muhammad Ismail <m.ismailusman47@gmail.com>
2025-12-07 18:00:07 +0500

    Implement Redis Caching Invalidation

--- src/controllers/product.controllers.js
+++ src/controllers/product.controllers.js
@@ -3,6 +3,7 @@ import Product from "../models/products.model.js";
 import { generateRedisCacheKey } from "../utlis/generate-cache-key.js";
 import { sendRes } from "../utlis/response-helper.js";
 
+// GET PRODUCTS FROM MONGODB ON EVERY REQUEST
 export const getAllProductsWithOutRedis = async (req, res) => {
 
      try {
@@ -18,14 +19,16 @@ export const getAllProductsWithOutRedis = async (req, res) => {
      }
 }
 
+// GET PRODUCTS FROM MONGODB ONCE AND THEN FROM REDIS ON EVERY REQUEST (UNTIL CACHE EXPIRES OR INVALIDATES)
 export const getAllProductsWithRedis = async (req, res) => {
-     const redisCacheKey = generateRedisCacheKey(req);
+     const redisCacheKey = generateRedisCacheKey(req, "api:v1:products");
      const cachedData = await redis.get(redisCacheKey);
 
      if (cachedData) {
           console.log("Cache hit");
           return sendRes(res, 200, "All products fetched successfully", JSON.parse(cachedData));
      }
+     console.log("Cache miss");
 
      try {
           const products = await Product.find();
@@ -35,6 +38,31 @@ export const getAllProductsWithRedis = async (req, res) => {
 
           return sendRes(res, 200, "All products fetched successfully", products);
 
+     } catch (error) {
+          console.log(error);
+          return sendRes(res, 500, "Internal server error");
+     }
+}
+
+// UPDATE PRODUCT NAME AND INVALIDATE CACHE PROGRAMATICALLY
+export const updateProductName = async (req, res) => {
+     try {
+          const { id } = req.params;
+          const { name } = req.body;
+
+          const product = await Product.findByIdAndUpdate(id, { name }, { new: true });
+
+          if (!product) return sendRes(res, 404, "Product not found");
+
+          // GET ALL REDIS KEYS THAT ARE RELATED TO PRODUCTS
+          const keys = await redis.keys("api:v1:products*");
+          if (keys.length > 0) {
+               await redis.del(keys);
+               console.log("PRODUCT CACHE INVALIDATED");
+               console.log("KEY PATTERN: api:v1:products*");
+          }
+
+          return sendRes(res, 200, "Product name updated successfully", product);
      } catch (error) {
           console.log(error);
           return sendRes(res, 500, "Internal server error");
--- src/routes/product.routes.js
+++ src/routes/product.routes.js
@@ -1,5 +1,6 @@
 import express from "express";
 import {
+    updateProductName,
     getAllProductsWithRedis,
     getAllProductsWithOutRedis,
 } from "../controllers/product.controllers.js";
@@ -8,5 +9,7 @@ const router = express.Router();
 
 router.get("/get-all-products-with-redis", getAllProductsWithRedis);
 router.get("/get-all-products-without-redis", getAllProductsWithOutRedis);
+
+router.patch("/update-product-name/:id", updateProductName);
 
 export default router;
